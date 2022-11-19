5a5a1ee248cac6225123e0c98323cc0d8cba51fe
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-11-19 09:52:03 +0000

    Add codes

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import initWebRoute from "./routes/web";
+import bodyParser from "body-parser";
+
+let app = express();
+
+// config view engine
+viewEngine(app);
+
+// init all web routes
+initWebRoute(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App is running at the port ${port}`);
+});
+

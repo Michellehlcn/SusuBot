cba4b5fd7ee569e78721b32ee3801214e206e11b
Michellehlcn <michellehlcn.au@gmail.com>
2022-11-27 23:25:12 +1100

    change wit@greetings to greeting

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,69 +1,69 @@
 'use strict';
 let getHomePage = (req, res) => {
-    return res.render("homepage.ejs")
+  return res.render("homepage.ejs")
 };
 // FIX ES6
-import {} from "dotenv/config";
+import { } from "dotenv/config";
 import { request } from "express";
 
 const MY_VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 let getWebHook = (req, res) => {
 
-    let VERIFY_TOKEN = MY_VERIFY_TOKEN; 
-    console.log("My verify token " + VERIFY_TOKEN);
-     // Parse the query params
-     let mode = req.query['hub.mode'];
-     let token = req.query['hub.verify_token'];
-     let challenge = req.query['hub.challenge'];
- 
-     // Checks if a token and mode is in the query string of the request
-     if (mode && token) {
- 
-         // Checks the mode and token sent is correct
-         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
- 
-             // Responds with the challenge token from the request
-             console.log('WEBHOOK_VERIFIED');
-             res.status(200).send(challenge);
- 
-         } else {
-             // Responds with '403 Forbidden' if verify tokens do not match
-             res.sendStatus(403);
-         }
-     }
- };
+  let VERIFY_TOKEN = MY_VERIFY_TOKEN;
+  console.log("My verify token " + VERIFY_TOKEN);
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+};
 
 let postWebHook = (req, res) => {
 
-     // Parse the request body from the POST
+  // Parse the request body from the POST
   let body = req.body;
   console.log(req.body);
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
     // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    body.entry.forEach(function (entry) {
 
       // Get the webhook event. entry.messaging is an array, but 
       // will only ever contain one event, so we get index 0
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
-    console.log("[MESSAGING] "+JSON.stringify(entry.messaging));
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      console.log("[MESSAGING] " + JSON.stringify(entry.messaging));
 
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);  
-    
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
-    // Check if the event is a message or postback and 
-    // pass the event to the appropriate handler function
-    if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message);
-    } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback);
-    }
-  });
+
+      // Check if the event is a message or postback and 
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
 
@@ -72,12 +72,9 @@ let postWebHook = (req, res) => {
     res.sendStatus(404);
   }
 };
-function firstTrait(nlp, name) {
-  return nlp && nlp.entities & nlp.traits[name] && nlp.traits[name][0];
-}
 
 // Handles messaging_postbacks events
-function handlePostback (sender_psid, received_postback) {
+function handlePostback(sender_psid, received_postback) {
   let response;
 
   // Get the payload for the postback
@@ -85,15 +82,15 @@ function handlePostback (sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === 'yes') {
-    response = { "text": "Thanks!"}
-  } else if ( payload === 'no' ){
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
   callSendAPI(sender_psid, response);
 };
 
 // Sends response messages via the Send API
-function callSendAPI (sender_psid, response) {
+function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
     "recipient": {
@@ -109,27 +106,32 @@ function callSendAPI (sender_psid, response) {
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
-    if(!err) {
+    if (!err) {
       console.log("message sent!");
     } else {
-      console.error("Unable to send message:"+ err);
+      console.error("Unable to send message:" + err);
     }
   });
 };
+
+function firstTrait(nlp, name) {
+  return nlp && nlp.entities & nlp.traits[name] && nlp.traits[name][0];
+}
+
 // Handles messages events
-function handleMessage (sender_psid, message){
+function handleMessage(sender_psid, message) {
   // handle message for react, like press like button
   // id like button
 
-  if (message && message.attachments && message.attachments[0].payload ) {
+  if (message && message.attachments && message.attachments[0].payload) {
     callSendAPI(sender_psid, "Thank you for viewing the catalogs");
     calSendAPIWithTemplate(sender_psid);
     return;
   }
 
-  let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye"];
+  let entitiesArr = ["greetings", "thanks", "bye"];
   let entityChosen = "";
-  entitiesArr.forEach( name => {
+  entitiesArr.forEach(name => {
     let entity = firstTrait(message.nlp, name);
     if (entity && entity.confidence > 0.8) {
       entityChosen = name;
@@ -140,15 +142,15 @@ function handleMessage (sender_psid, message){
     //default
     callSendAPI(sender_psid, `The bot is needed more training, try to say "thanks a lot" or "hi" to the bot`);
   } else {
-    if (entityChosen === "wit$greeting") {
+    if (entityChosen === "greeting") {
       // send greeting message
       callSendAPI(sender_psid, "Hi there! this is SusuBot, Welcome to Susu Shop");
     }
-    if (entityChosen === "wit$thanks") {
+    if (entityChosen === "thanks") {
       // send thanks message
       callSendAPI(sender_psid, "You're welcome");
     }
-    if (entityChosen === "wit$bye") {
+    if (entityChosen === "bye") {
       // send bye message
       callSendAPI(sender_psid, 'bye-bye!');
     }
@@ -190,19 +192,50 @@ let calSendAPIWithTemplate = (sender_psid) => {
 
   request({
     "uri": "https://graph.facebook.com/v15.0/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": body
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent');
     } else {
-      console.error("Unable to send message: "+ err);
+      console.error("Unable to send message: " + err);
     }
   });
 };
 export {
-    getHomePage,
-    getWebHook,
-    postWebHook
+  getHomePage,
+  getWebHook,
+  postWebHook
 };
+
+// Same response
+// [MESSAGING]
+// [{ 
+//   "sender": { "id": "4518690248161445" }, 
+//   "recipient": { "id": "1787439031468531" }, 
+//   "timestamp": 1669550325076, 
+//   "message": { 
+//     "mid": "m_2GBMvD7GJOOCbu11FmktxiD_b6zWZ0U6iABKQ6vmClv0DgF_vL6SxCogG5-lvSpXqoyI4o5IHnsCNun6yBdpLA", 
+//     "text": "hello", 
+//     "nlp": { 
+//       "entities": 
+//       { 
+//         "sentiment": 
+//         [{ 
+//           "confidence": 0.54351806640625, 
+//           "value": "positive" 
+//         }], 
+//         "greetings": 
+//         [{ 
+//           "confidence": 0.99988770484924, 
+//           "value": "true" 
+//         }] 
+//       }, 
+//       "detected_locales": 
+//       [{ 
+//         "locale": "en_XX", 
+//         "confidence": 0.5896 
+//       }] 
+//     } }
+//    }]

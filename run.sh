4250a2964887880f84545d5ce4d7dc5b84b2659e
michellelhcn <michellehlcn.au@gmail.com>
2022-12-21 20:31:09 +1100

    update templates

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ TEST application
 GET https://susu-bot.herokuapp.com/webhook?hub.mode=subscribe&hub.verify_token=37d587b399b1cf36e2adad15fc0f0eed&hub.challenge=1158201444
 
 curl -H "Content-Type: application/json" -X POST "https://susu-bot.herokuapp.com/webhook" -d '{"object": "page", "entry": [{"messaging": [{"message": "TEST_MESSAGE"}]}]}'
+
+curl -X POST -H "Content-Type: application/json" -d '{"recipient":{"id":"4518690248161445"},"message":"alo"}' "https://graph.facebook.com/v8.0/me/messages?access_token=EAAt6rK53z2IBALpBFriWfSiEH7hSR8UvPgsaoWol9Xe39uE5LPEprrsrE1AzLGdkuhG76mBlcjUuzpt6nju4H9OdN7jiQiXBTpCy0pYZCylyMLPvZBi5y04jDp3P1x7SxAulGUKOgxiXcKK9Ky7PENLkewfCh8m2koxsahb4Q6g8DK6AZCK"
